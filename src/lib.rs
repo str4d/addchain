3788155e653bbca9b78976429f38482cf5b40bdb
Jack Grigg <thestr4d@gmail.com>
2019-12-19 18:13:13 -0600

    Initial implementation using the Bergeron-Berstel-Brlek-Duboc algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! Library for generating addition chains.
+
+use num_bigint::BigUint;
+
+mod bbbd;
+
+/// Returns the shortest addition chain we can find for the given number, using all
+/// available algorithms.
+pub fn find_shortest_chain(n: BigUint) -> Vec<BigUint> {
+    bbbd::find_shortest_chain(n)
 }
